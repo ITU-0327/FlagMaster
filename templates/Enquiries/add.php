f12d6eda167321f7a2fbcab9077bb35d74f50757
ganglin <glin0011@student.monash.edu>
2024-09-03 22:49:42 +1000

    complete add data for contact us

--- templates/Enquiries/add.php
+++ templates/Enquiries/add.php
@@ -1,54 +1,32 @@
 <?php
 /**
- * @var \App\View\AppView $this
- * @var \App\Model\Entity\Enquiry $enquiry
+
+@var \App\View\AppView $this
+@var \App\Model\Entity\Enquiry $enquiry
+@var \Cake\Collection\CollectionInterface|string[] $users
  */
 ?>
-
-<div class="d-flex justify-content-center align-items-center vh-100">
-    <div class="col-md-6">
-        <h1 class="text-center mb-4">Contact Us</h1>
-        <?= $this->Form->create($enquiry) ?>
-        <div class="form-group mb-3">
-            <?= $this->Form->label('first_name', 'First Name:') ?>
-            <?= $this->Form->text('first_name', ['class' => 'form-control']) ?>
+<div class="row">
+    <aside class="column">
+        <div class="side-nav">
+            <h4 class="heading"><?= ('Actions') ?></h4>
+            <?= $this->Html->link(('List Enquiries'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
         </div>
-        <div class="form-group mb-3">
-            <?= $this->Form->label('last_name', 'Last Name:') ?>
-            <?= $this->Form->text('last_name', ['class' => 'form-control']) ?>
+    </aside>
+    <div class="column column-80">
+        <div class="enquiries form content">
+            <?= $this->Form->create($enquiry) ?>
+            <fieldset>
+                <legend><?= __('Add Enquiry') ?></legend>
+                <?php
+                echo $this->Form->control('first_name', ['label' => 'First Name']);
+                echo $this->Form->control('last_name', ['label' => 'Last Name']);
+                echo $this->Form->control('email', ['label' => 'Email']);
+                echo $this->Form->control('message', ['label' => 'Message', 'type' => 'textarea']);
+                ?>
+            </fieldset>
+            <?= $this->Form->button(__('Submit')) ?>
+            <?= $this->Form->end() ?>
         </div>
-        <div class="form-group mb-3">
-            <?= $this->Form->label('phone', 'Phone Number:') ?>
-            <?= $this->Form->text('phone', ['class' => 'form-control']) ?>
-        </div>
-        <div class="form-group mb-3">
-            <?= $this->Form->label('message', 'Message:') ?>
-            <?= $this->Form->textarea('message', ['class' => 'form-control']) ?>
-        </div>
-        <div class="form-group text-center">
-            <?= $this->Form->button(__('Send'), ['class' => 'btn btn-primary']) ?>
-        </div>
-        <?= $this->Form->end() ?>
     </div>
 </div>
-
-<style>
-    .vh-100 {
-        height: 100vh;
-    }
-
-    .form-control {
-        border-radius: 0.375rem;
-        padding: 0.75rem;
-        font-size: 1rem;
-    }
-
-    .btn {
-        padding: 0.75rem 1.5rem;
-        font-size: 1rem;
-        color: #fff;
-        border: none;
-        border-radius: 0.375rem;
-        transition: background-color 0.2s ease-in-out;
-    }
-</style>
